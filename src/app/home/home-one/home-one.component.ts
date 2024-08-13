1ad7acc13654dc5311564e55cc4eca846d960554
julianR92 <julianrincon9230@gmail.com>
2024-08-13 14:01:29 -0500

    first-commit

---
+++ src/app/home/home-one/home-one.component.ts
@@ -0,0 +1,63 @@
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
+import { Pagination,EffectFade, Autoplay } from 'swiper/modules';
+import { ProductService } from 'src/app/shared/services/product.service';
+import { IProduct } from 'src/app/shared/types/product-d-t';
+import { IHeroSlider } from 'src/app/shared/types/hero-slider-t';
+import { HeroSliderData } from 'src/app/shared/data/hero-slider-data';
+
+Swiper.use([Pagination, EffectFade]);
+
+
+@Component({
+  selector: 'app-home-one',
+  templateUrl: './home-one.component.html',
+  styleUrls: ['./home-one.component.scss']
+})
+
+export class HomeOneComponent implements AfterViewInit  {
+  @ViewChild('heroSliderContainer') heroSliderContainer!: ElementRef;
+  public swiperInstance: Swiper | undefined;
+  public hero_slider_data: IHeroSlider[] = HeroSliderData.hero_slider_one;
+  public trendingProducts: IProduct[] = [];
+  public bannerProducts: IProduct[] = [];
+  public discountProducts: IProduct[] = [];
+  public perView: number = 8;
+
+  constructor(private productService: ProductService) {
+    this.productService.products.subscribe((products) => {
+      this.trendingProducts = products.filter((p) => p.trending);
+      this.bannerProducts = products.filter((p) => p.banner).slice(0, 2);
+      this.discountProducts = products.filter((p) => p.discount! > 0).slice(0,5);
+    });
+  }
+
+  // handle per view
+  handlePerView() {
+    this.perView = this.perView + 4;
+  }
+
+  ngAfterViewInit() {
+    if (this.heroSliderContainer) {
+      this.swiperInstance = new Swiper('.slider-active', {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        effect : 'fade',
+        modules:[Pagination,EffectFade],
+        pagination: {
+          clickable: true,
+          el:'.tp-slider-dot'
+        },
+        autoplay: {
+          delay: 5000, // Tiempo en milisegundos entre cambios de diapositivas
+          disableOnInteraction: false,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      })
+    }
+  }
+}
